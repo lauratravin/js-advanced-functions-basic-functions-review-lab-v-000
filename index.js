1036f2c6d1cf314b5820226cbdc569b704b65b92
Laura Messa Travin <laura.travin@gmail.com>
2021-05-03 21:18:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,3 +31,13 @@ const Calculator = {
     return a / b;
   }
 }
+
+let actionApplyer = function(start, ray) {
+  let a = start
+
+  for (let i = 0; i < ray.length; i++ ){
+    a = ray[i](a)
+  }
+
+  return a
+}
